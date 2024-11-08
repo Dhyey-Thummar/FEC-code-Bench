a2769a5dd49b35d0b81e5d7cfa2fbe51c93b17de
pratyush401 <pratnps@gmail.com>
2024-11-08 14:50:19 -0500

    Simulate dropping channel

--- sim.py
+++ sim.py
@@ -1,6 +1,9 @@
 import socket
 import sys
 import time
+import random
+import numpy as np
+import math
 
 def receive_blocks_from_input(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -95,17 +98,73 @@ def send_blocks_to_output(blocks, host, port, file_name, file_length, block_size
     s.sendall("END".encode())
     s.close()
 
-def run_sim(host, receive_input_port, send_output_port):
+def run_sim(host, receive_input_port, send_output_port, channel_param, probability):
     blocks, block_size, file_name, file_length, block_num_size = receive_blocks_from_input(host, receive_input_port)
-    blocks = sim_channel(blocks)
+    blocks = sim_channel(blocks, channel_param)
     send_blocks_to_output(blocks, host, send_output_port, file_name, file_length, block_size, block_num_size, len(blocks))
 
-def sim_channel(blocks):
-    # return blocks[::-1]
-    return blocks
+def sim_channel(blocks, channel, probability, std):
+    match channel:
+        case 0:
+            # No dropping
+            print("Not dropping any blocks.")
+            return blocks
+        case 1:
+            # No dropping but only rearranging
+            seed = random.randint(50)
+            k = 0
+            random.seed(seed)
+            for i in range(50):
+                x = random.randint(0, len(blocks))
+                y = random.randint(0, len(blocks))
+                if x != y:
+                    k = k + 1
+                    tmp = blocks[x]
+                    blocks[x] = blocks[y]
+                    blocks[y] = tmp
+            print("Rearranging ", k, "blocks.")
+            return blocks
+        case 2:
+            # Dropping with probability BEC
+            new_blocks = []
+            k = 0
+            for b in range(0, len(blocks)):
+                if (random.uniform(0, 1) >= probability):
+                    k = k + 1
+                    new_blocks.append(blocks[b])
+            print("Dropping ", len(blocks) - k, "blocks.")
+            return new_blocks
+        case 3:
+            # Dropping with Additive White Gaussian Noise
+            new_blocks = []
+            k = 0
+            prob_norm_function = np.random.normal(loc=0, scale=std, size=len(blocks))
+            for b in range(0, len(blocks)):
+                if abs(prob_norm_function[b]) < std:
+                    k = k + 1
+                    new_blocks.append(blocks[b])
+            print("Dropping ", len(blocks) - k, "blocks.")
+            return new_blocks
+        case 4:
+            # Dropping using the Rayleigh fading channel
+            additive_norm = np.random.normal(loc=0, scale=std, size=len(blocks))
+            multiplicative_norm = np.random.normal(loc=0, scale=(1/math.sqrt(2)), size=len(blocks))
+            new_blocks = []
+            k = 0
+            for b in range(0, len(blocks)):
+                if abs(additive_norm[b]) < std:
+                    if abs(1 - abs(multiplicative_norm[b])) < 0.5:
+                        k = k + 1
+                        new_blocks.append(b)
+            print("Dropping ", len(blocks) - k, "blocks.")
+            return new_blocks
 
 if __name__ == '__main__':
+    channel_param = int(sys.argv[0])
+    channel_prob = int(sys.argv[1])
+    assert(channel_param >= 0)
+    assert(channel_param <= 4)
     host = 'localhost'
     receive_input_port = 12345
     send_output_port = 12346
-    run_sim(host, receive_input_port, send_output_port)
+    run_sim(host, receive_input_port, send_output_port, channel_param, channel_prob)
